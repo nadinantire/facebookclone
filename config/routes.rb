fdf0b5e092453b784cf0090fa49ee4c08815c26b
nadinantire <nadinantire@gmai..com>
2019-09-03 05:00:02 +0000

    facebookclone task

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :blogs
+  root 'home#index'
+  resources :blogs
+  devise_for :users, controllers: {
+    registrations: "registrations"
+  }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
