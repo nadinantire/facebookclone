d9b6946fcee27b29f42bbe20cdb290061f4cb5cf
nadinantire <nadinantire@gmail.com>
2019-09-05 07:07:46 +0000

    CRUD function, edit profile image, prevent other people's posts, login function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_124458) do
+ActiveRecord::Schema.define(version: 2019_09_04_125536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2019_09_02_124458) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "image"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -45,4 +53,5 @@ ActiveRecord::Schema.define(version: 2019_09_02_124458) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "profiles", "users"
 end
